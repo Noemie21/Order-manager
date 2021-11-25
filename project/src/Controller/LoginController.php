f845151c0cebe7d8600cffaee657836c65a909ba
Nasake <wilfrid.salles-otta@supinternet.fr>
2021-11-25 12:59:56 +0100

    login ( updated )

--- project/src/Controller/LoginController.php
+++ project/src/Controller/LoginController.php
@@ -3,16 +3,25 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class LoginController extends AbstractController
 {
     #[Route('/login', name: 'login')]
-    public function index(): Response
+
+    public function index(AuthenticationUtils $authenticationUtils): Response
     {
-        return $this->render('login/index.html.twig', [
-            'controller_name' => 'LoginController',
+
+         $error = $authenticationUtils->getLastAuthenticationError();
+
+         $lastUsername = $authenticationUtils->getLastUsername();
+
+        return $this->render('login/login.html.twig', [
+             'last_username' => $lastUsername,
+             'error'         => $error,
         ]);
+
     }
 }
--- project/src/Form/LoginFormType.php
+++ project/src/Form/LoginFormType.php
@@ -6,6 +6,7 @@ use App\Entity\Login;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 
 class LoginFormType extends AbstractType
 {
@@ -14,6 +15,7 @@ class LoginFormType extends AbstractType
         $builder
             ->add('email')
             ->add('password')
+            ->add('submit', SubmitType::class)
         ;
     }
 
